1cd4cf9bc3fc6779c42a8f8d3a817eab8be37005
h4zm1 <mosbah.hazmi@gmail.com>
2025-12-14 17:41:57 +0100

    Fix: remove unnecessary @transactional annotations

--- src/main/kotlin/com/example/everlookcalendar/service/EventService.kt
+++ src/main/kotlin/com/example/everlookcalendar/service/EventService.kt
@@ -3,19 +3,12 @@ package com.example.everlookcalendar.service
 import com.example.everlookcalendar.data.Event
 import com.example.everlookcalendar.repository.EventRepo
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
+//@Transactional
 class EventService(private val repository: EventRepo) {
 
     fun getAllEvents(): List<Event> =
             repository.queryAllEvents()
 
-    fun saveEvents(list:List<Event>){
-        repository.saveAll(list)
-    }
-    fun saveEvent(event : Event){
-        repository.save(event)
-    }
 }
